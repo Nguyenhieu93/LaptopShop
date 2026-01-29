067d04ea9faddb516a7626ad8610c5ec5da8d8a7
hieu <nguyenminhhieuclls@gmail.com>
2026-01-29 14:50:54 +0700

    Add dependencies for Spring Boot DevTools, JPA, MySQL, and Security; update HelloController with user and admin endpoints; enhance LaptopshopApplication to list bean definitions; create MyService class.

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -8,7 +8,17 @@ public class HelloController {
 
     @GetMapping("/")
     public String index() {
-        return "Hello World with Hieu Nguyen!";
+        return "Hello World!";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only user can access this page!";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "only admin";
     }
 
 }
